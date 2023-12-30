47f7c07e7d482bf25de3e2fbdc4751d748f61971
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2023-12-30 19:25:50 +0600

    var ler and const

---
+++ day_1/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = "14215";
+let accountEmail = "mehedihasanfoysal22@gmail.com";
+var accountPassword = "85475";
+
+accountEmail = "jani@gmail.com";
+accountPassword = "88888888888";
+
+/*
+
+prefer not to use var 
+becouse of issue in block scope and functional scipe
+
+*/
+
+console.table([accountId, accountEmail, accountPassword]);
